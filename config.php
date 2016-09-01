396a55d3d452ef6b35ae970190e09ab6aea9ee16
Fuzzy <fuzzymannerz@users.noreply.github.com>
2016-09-01 19:21:02 +0200

    Comments instruction tweak

--- config.php
+++ config.php
@@ -1,23 +1,20 @@
 <?php
 
-/*******************************************************************************/
-/** to configure swmp copy this file to config.local.php and change the values */
-/*******************************************************************************/
+/*********************************************************************************/
+/** To configure SWMP copy this file to "config.local.php" and change the values */
+/*********************************************************************************/
 
 return array
 (
-    // all themes are files in the /css/ folder
-    // available themes are:
-    // "cerulean", "cosmo", "lumen", "paper", "readable", "sandstone",
-    // "simplex", "slate", "spacelab", "superhero", "united", "yeti",
-    // "darkplex", "rose", "terminal"
+    // All themes are files in the "css/themes" folder
+    // The default theme is "simplex"
     "theme" => "simplex",
 
-    // if set to true errors are shown for system information that could not be recieved
-    // after the server is set up it is recommended to set this to false
+    // If set to true, errors are shown for system information that could not be retrieved
+    // After the server is set up it is recommended to set this to false
     "show_errors" => true,
 
-    // the windowtitle
-    // available replacements are {hostname}, {ip}, {os} and {kernel}
+    // The window title
+    // Available replacements are: {hostname}, {ip}, {os} and {kernel}
     "window_title" => "SWMP | {hostname}",
 );
